cd4542729ac39af97470e09d635a976b3a76fe38
Wen Huey <joeychew52@gmail.com>
2022-09-24 20:46:56 +0800

    Login, Signup connect database, insert, css

--- ProjectPool/Controllers/AccountController.cs
+++ ProjectPool/Controllers/AccountController.cs
@@ -4,11 +4,20 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Data.SqlClient;
 
 namespace ProjectPool.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly IConfiguration _configuration;
+
+        public AccountController (IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         [Route("Signup")]
         public IActionResult SignUp()
         {
@@ -17,21 +26,42 @@ namespace ProjectPool.Controllers
 
         [Route("Signup")]
         [HttpPost]
-        public IActionResult SignUp(SignUpModel signUpModel)
+        public IActionResult SignUp(SignUpModel model)
         {
-            if(ModelState.IsValid)
-            {
-                //write code
+            //Connect db
+            string connStr = _configuration.GetConnectionString("DefaultConnection");
+            SqlConnection conn = new SqlConnection(connStr);
+            conn.Open();
 
-                ModelState.Clear();
-            }
-            return View();
+            //Create command
+            string query = "Insert into Employer (FirstName, LastName, Email, [Password]) VALUES (@FirstName, @LastName, @Email, hashbytes('sha2_512', @Password))";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@FirstName", model.FirstName);
+            cmd.Parameters.AddWithValue("@LastName", model.LastName);
+            cmd.Parameters.AddWithValue("@Email", model.Email);
+            cmd.Parameters.AddWithValue("@Password", model.Password);
+
+            //Execute query
+            cmd.ExecuteNonQuery();
+
+            //close connection
+            conn.Close();
+
+            return View("Login");
         }
+
 
         [Route("/")]
         public IActionResult Login()
         {
             return View();
         }
+
+        //[Route("/")]
+        //[HttpPost]
+        //public IActionResult Login()
+        //{
+        //    return View();
+        //}
     }
 }
--- ProjectPool/Models/SignUpModel.cs
+++ ProjectPool/Models/SignUpModel.cs
@@ -8,20 +8,23 @@ namespace ProjectPool.Models
 {
     public class SignUpModel
     {
-        [Required(ErrorMessage ="Please enter your name")]
-        public string Name { get; set; }
+        [Required(ErrorMessage ="Please enter your first name")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Please enter your last name")]
+        public string LastName { get; set; }
 
         [Required(ErrorMessage = "Please enter your email")]
         public string Email { get; set; }
 
         [Required(ErrorMessage = "Please enter a strong password")]
-        [Compare("ConfirmPassword", ErrorMessage ="Password does not match")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
         [Required(ErrorMessage = "Please re-enter your password")]
         [Display(Name ="ConfirmPassword")]
         [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Password does not match")]
         public string ConfirmPassword { get; set; }
 
     }
