c4e5a3a5f3bf85b2c4c95404823e38098a794475
Wen Huey <joeychew52@gmail.com>
2022-09-27 21:59:31 +0800

    Create project, form action, @model, delete ajax

---
+++ ProjectPool/Controllers/ProfileController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectPool.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectPool.Controllers
+{
+    public class ProfileController : Controller
+    {
+        [Route("CreateProfile")]
+        public IActionResult CreateProfile()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //[Route("CreateProfile")]
+        //public IActionResult CreateProfile()
+        //{
+        //    return View();
+        //}
+
+
+    }
+}
---
+++ ProjectPool/Models/CreateProjectModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectPool.Models
+{
+    public class CreateProjectModel
+    {
+
+        [Required(ErrorMessage = "This field is required")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "This field is required")]
+        public string Description { get; set; }
+        [Required]
+        public int DurationFrom { get; set; }
+        [Required]
+        public int DurationTo { get; set; }
+        [Required]
+        public string ConfirmPassword { get; set; }
+        [Required]
+        public string Status { get; set; }
+        [Required]
+        public int Cost { get; set; }
+        [Required]
+        public DateTime DatePosted { get; set; }
+
+        public int EmployerID { get; set; }
+
+        //for other table
+        [Required]
+        public int CategoryID { get; set; }
+        [Required]
+        public int SubCategoryID { get; set; }
+        [Required]
+        public string Skill { get; set; }
+        [Required]
+        public string Language { get; set; }
+
+    }
+}
